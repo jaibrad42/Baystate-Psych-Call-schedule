bc5c2bf067d6c78cc9091360031410a08a1506f5
jaibrad42 <jaibrad42@gmail.com>
2026-06-02 19:43:50 -0400

    Update event listener to use history.pushState

--- streamlit_app.py
+++ streamlit_app.py
@@ -1062,7 +1062,7 @@ with tab_cal:
             "var d=this.getAttribute('data-edit-date');"
             "var r=this.getAttribute('data-edit-role');"
             "var u=window.parent.location.origin+window.parent.location.pathname+'?edit_date='+d+'&edit_role='+r;"
-            "window.parent._eu=u;window.parent.eval("'window.location.assign(window._eu)'");"
+            "window.parent.history.pushState({},'',u);window.parent.location.reload();"
             "});"
             "});"
             "}catch(e){}"
